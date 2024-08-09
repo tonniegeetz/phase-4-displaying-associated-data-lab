b3c52fb89b90e91cca2d7819db20783cda072120
tonniegeetz <tonniegeetz@gmail.com>
2024-08-09 13:47:30 +0300

    use of include in displaying associated data

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,10 @@
 class UsersController < ApplicationController
+  def show
+    user = User.find(params[:id])
+    if user
+      render json: user, include: :items, status: :ok
+    else
+      render json: { error: 'user not found' }, status: :not_found
+    end
+  end
 end
